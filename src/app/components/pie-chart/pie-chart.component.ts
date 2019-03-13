15cb839e41d1d6fe2a6b5f47be608bfaf4a796c2
Yurii Cherkun <cherkun.ura@gmail.com>
2019-03-14 01:00:08 +0200

    [dev] work over pie-chart

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -11,29 +11,50 @@ import * as dc from 'dc';
 export class PieChartComponent implements OnInit {
   sourceData;
   appCrossfilter;
-  count;
   constructor() { }
   
   drowPie(){
-    this.sourceData = d3.csv('../../assets/data.csv').then((experiment)=>{
-      this.appCrossfilter = crossfilter(experiment);
-      this.count = this.appCrossfilter.dimension(d=>d.Item_code);
+    // get data from csv file
+    this.sourceData = d3.csv('../../assets/data.csv').then((data)=>{
+      ////
+        data.forEach(function(d) {
+          console.log(d);
+        });
+      /////
+      this.appCrossfilter = crossfilter(data);
+      const categoryDimension = this.appCrossfilter.dimension(d=>d["item_category"]);
+      const categoryGroup = categoryDimension.group();
+      const cetegoryFiltered = categoryDimension.filter('EE');
+      // this.print_filter(cetegoryFiltered);
       const pieChart = dc.pieChart('#pie');
       const markdownDimension = this.appCrossfilter.dimension(data=>data.markdown);
       const markdownGroup = markdownDimension.group().reduceCount();
       pieChart.width(500)
               .height(500)
-              .dimension(markdownDimension)
-              .group(markdownGroup)
+              .dimension(categoryDimension)
+              .group(categoryGroup)
               .on('renderlet', (chart)=>{
                 chart.selectAll('rect').on('click', (d)=>{
                   console.log('click!', d);
                 })
-              })
+              });
+      const lineChart = dc.lineChart('#lineChart');
+      lineChart.width(1200)
+                .height(300)
+                .x(d3.scaleLinear().domain([20,35]))
+                .y(d3.scaleLinear().domain([10,20]))
+                .dimension(markdownDimension)
+                .group(markdownGroup);
       dc.renderAll();
     });
-    
   }  
+  print_filter(filter){
+    var f=eval(filter);
+    if (typeof(f.length) != "undefined") {}else{}
+    if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
+    if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
+    console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+  } 
   ngOnInit() {
     
     this.drowPie();
